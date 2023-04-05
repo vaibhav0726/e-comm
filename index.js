c53bae60ee811120660b23a056a936fcd0f93f77
vaibhav0726 <vaibhav.varshney2_cs20@gla.ac.in>
2023-04-05 19:59:24 +0530

    done

--- index.js
+++ index.js
@@ -1,106 +1,89 @@
-if(process.env.NODE_ENV != "production"){
-
-  require("dotenv").config({ path: "./config.env"})
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 const express = require("express");
-const path = require("path")
-const engine = require("ejs-mate")
+const path = require("path");
+const engine = require("ejs-mate");
 const app = express();
+// const PORT = 3000;
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const session = require("express-session")
+const session = require("express-session");
 const flash = require("connect-flash");
-const User = require("./models/User")
+const User = require("./models/User");
 const passport = require("passport");
-var LocalStrategy = require('passport-local');
+var LocalStrategy = require("passport-local");
 
-const dbUrl = process.env.DB_URI 
+const dbUrl = process.env.DB_URI || "mongodb://localhost/shopping-cart";
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080;
 
-const sessionSecret = process.env.SESSION_SECRET || 'this is a secret session'
+const sessionSecret = process.env.SESSION_SECRET || "this is a secret session";
 
 //Connect to DB
-mongoose.connect(dbUrl, { useNewUrlParser: true,useUnifiedTopology: true })
-.then(()=> console.log(" DB CONNECTED!"))
-.catch((err)=> console.log(err));
 
-
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(" DB CONNECTED!"))
+  .catch((err) => console.log(err));
 
 const sessionflash = {
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+  },
+};
 
-      httpOnly:true,
-      expires: Date.now()  + 7 *24*60*60*1000
-    }
-  };
-  
-  app.use(session(sessionflash))
-  app.use(flash());
-  app.use(passport.authenticate('session'));
+app.use(session(sessionflash));
+app.use(flash());
+app.use(passport.authenticate("session"));
 
-
-  app.use((req, res, next) => {
-
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    res.locals.currentUser = req.user;
-    next();
-
-  });
-  
-
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // All Product Routes
 const productRouter = require("./routes/productRoutes");
 
 //Review Routes
 
-const reviewRouter = require("./routes/reviewRoutes")
+const reviewRouter = require("./routes/reviewRoutes");
 
 // Auth router
 
-const authRouter = require("./routes/authRoutes")
-
+const authRouter = require("./routes/authRoutes");
 
 // Middlewares
-app.use(methodOverride('_method'));
-app.use(express.urlencoded({extended:true}));
-app.engine("ejs", engine)
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname, "views" ))
-app.use(express.static(path.join(__dirname,'public')))
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-// Passport 
+// Passport
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
 //home route
-app.get("/", (req,res)=>{
-
-    res.render("index")
-})
-
-
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // Routers
 
-app.use( productRouter); 
+app.use(productRouter);
 app.use(reviewRouter);
 app.use(authRouter);
 
-
-
-app.listen(port, ()=>{
-
-    console.log(` server running at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(` server running at port ${port}`);
+});
